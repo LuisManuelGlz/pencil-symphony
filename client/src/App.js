72902527a3d0fdf4ca2fdda8a03a08473cd43a2c
Luis Manuel <mannyglzg@gmail.com>
2020-01-10 11:01:29 -0800

    Se creó el cliente

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import Navation from './components/Navigation';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <Navation />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
